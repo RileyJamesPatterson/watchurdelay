14ae336d7e076b91c09acecc3b4b42eb53ceed18
Riley Patterson <Rileyjamespatterson@gmail.com>
2023-03-03 11:34:39 -0800

    Added pivot chart placeholders to index page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from dashboard.models import Flight
+from django.core import serializers
+from django.http import JsonResponse
 
 def index(request):
-    return HttpResponse("Index has been reached")
+    return render(request,'home.html',{})
+
+def pivot_data(request):
+    'a method to serve pivot table data'
+    dataset = Flight.objects.filter(OriginCityName="Atlanta, GA") #HARDCODED FILTER FOR NOW TO REDUCE DATA SIZE
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
